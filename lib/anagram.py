153c9b1ecad5979b536be8800064d763130ebbc0
Eman Gurung <egurung1117@gmail.com>
2023-09-13 07:09:46 -0400

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
-# your code goes here!
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        matchingWords = []
+        charList = list(self.word)
+        charList.sort()
+        for word in words:
+            charList2 = list(word)
+            charList2.sort()
+            if charList == charList2:
+                matchingWords.append(word)
+        return matchingWords
+    
+anagram1 = Anagram("word")
+print(anagram1.match(["hello", "goodbye", "dowr", "drow", "wdro"]))
